d3a4ef22cba947237f49bb5aba5b7de9b61991f4
x0rus <149977404+x0rus@users.noreply.github.com>
2024-05-11 21:34:18 +0300

    modified:   controllers/main.js

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,6 @@
 const staticFile = require("../appModules/http-utils/static-file");
 const { getData, endpoins} = require('../appModules/api')
-const { makeRatingFile, config } = require('../appModules/rating')
+const { config, makeRatingFile } = require('../appModules/rating')
 
 async function mainRouteController(res, publicUrl, extname) {
     const data = await getData(endpoins.games);
