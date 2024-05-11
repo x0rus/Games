c8166583e77821e7f76400893b7325c4ac711ce4
x0rus <149977404+x0rus@users.noreply.github.com>
2024-05-11 20:24:09 +0300

    deleted:    app.js 	deleted:    appModules/http-utils/mime-types.js 	deleted:    appModules/http-utils/parse-body.js 	deleted:    appModules/http-utils/static-file.js 	deleted:    controllers/default.js 	deleted:    controllers/game.js 	deleted:    controllers/main.js 	deleted:    controllers/vote.js 	deleted:    dataset/rating.json 	deleted:    public/fonts/anonymouspro-bold.woff 	deleted:    public/fonts/anonymouspro-regular.woff 	deleted:    public/fonts/rubic-one.woff 	deleted:    public/index.html 	deleted:    public/scripts/script.js 	deleted:    public/styles/style.css

--- controllers/main.js
+++
@@ -1,8 +0,0 @@
-const staticFile = require("../appModules/http-utils/static-file");
-
-async function mainRouteController(res, publicUrl, extname) {
-    res.statusCode = 200;
-    staticFile(res, publicUrl, extname);
-}
-
-module.exports = mainRouteController;
