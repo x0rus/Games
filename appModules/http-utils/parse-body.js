5d67c0e6aa634ebea2e5ce89c37da38cd631ebe1
x0rus <149977404+x0rus@users.noreply.github.com>
2024-05-11 19:36:58 +0300

    Add files via upload
    	new file:   app.js
    	new file:   appModules/http-utils/mime-types.js
    	new file:   appModules/http-utils/parse-body.js
    	new file:   appModules/http-utils/static-file.js
    	new file:   controllers/default.js
    	new file:   controllers/game.js
    	new file:   controllers/main.js
    	new file:   controllers/vote.js
    	new file:   dataset/rating.json
    	new file:   public/fonts/anonymouspro-bold.woff
    	new file:   public/fonts/anonymouspro-regular.woff
    	new file:   public/fonts/rubic-one.woff
    	new file:   public/index.html
    	new file:   public/scripts/script.js
    	new file:   public/styles/style.css

---
+++ appModules/http-utils/parse-body.js
@@ -0,0 +1,16 @@
+
+
+function parseBody(req, callback) {
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk.toString();
+    });
+    req.on("end", () => {
+        callback(null, body);
+    });
+    req.on("error", (error) => {
+        callback(error, null);
+    });
+}
+
+module.export = parseBody;
---
+++ controllers/main.js
@@ -0,0 +1,8 @@
+const staticFile = require("../appModules/http-utils/static-file");
+
+async function mainRouteController(res, publicUrl, extname) {
+    res.statusCode = 200;
+    staticFile(res, publicUrl, extname);
+}
+
+module.exports = mainRouteController;
