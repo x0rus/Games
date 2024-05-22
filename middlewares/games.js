b67849d5edab0f34dcb0b89438d0ae455825b67c
x0rus <149977404+x0rus@users.noreply.github.com>
2024-05-22 19:00:32 +0300

    Update games.js

--- middlewares/games.js
+++ middlewares/games.js
@@ -1,36 +1,35 @@
-const {readData, writeData} = require('../utils/data');
+const { readData, writeData } = require('../utils/data');
 
-const getAllGames = async(req, res, next)=>{
+const getAllGames = async (req, res, next) => {
     const games = await readData("./data/games.json");
-if (!games) {
-    res.status(400);
-    res.send({
-    status: "error",
-    message: "Нет игр в базе данных. Добавьте игру."
-    });
-    return;
-}
-req.games = games;
-next()
+    if (!games) {
+        res.status(400);
+        res.send({
+            status: "error",
+            message: "Нет игр в базе данных. Добавьте игру."
+        });
+        return;
+    }
+    req.games = games;
+    next()
 };
 
 const allowedCors = [
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
     'http://localhost:3000'
-  ];
+];
 
-  function cors(req, res, next) {
+function cors(req, res, next) {
     const { origin } = req.headers;
-    
     if (allowedCors.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
     }
-    
+
     next();
 };
 
-module.exports = { 
+module.exports = {
     getAllGames,
     cors
- };
+};
